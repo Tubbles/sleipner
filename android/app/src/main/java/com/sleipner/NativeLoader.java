9751967d9c578d2546ef935f4b5de7582eaeb4ef
Tubbles <jae91m@gmail.com>
2026-03-16 19:25:41 +0100

    Request all-files-access permission on Android 11+
    Without runtime permission request, the app silently fails to read
    gamedata.toml from the Syncthing folder. On first launch the user
    will be sent to Settings to grant "All files access".

--- android/app/src/main/java/com/sleipner/NativeLoader.java
+++ android/app/src/main/java/com/sleipner/NativeLoader.java
@@ -1,9 +1,26 @@
 package com.sleipner;
 
 import android.app.NativeActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 
 public class NativeLoader extends NativeActivity {
     static {
         System.loadLibrary("sleipner");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
+                && !Environment.isExternalStorageManager()) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+        }
+        super.onCreate(savedInstanceState);
     }
 }
